74f5aa01f6b6b59383a3e55b5dcf5f8d03be0c64
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-13 16:43:14 -0400

    added view department

--- index.js
+++ index.js
@@ -189,5 +189,23 @@ function viewEmployees() {
         loadPrompts();
     })
 }
+
+function viewDepartments() {
+    var query = "SELECT name FROM department"
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        loadPrompts();
+    })
+}
+
+function viewDepartments() {
+    var query = "SELECT name FROM department"
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        loadPrompts();
+    })
+}
 // DB.viewAllEmployees
 // module.exports = connection;
